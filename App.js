9a8cadfe3a26b415cde9629e92d46845d9d27b5b
Tom Daniel <tomdaniel.it@gmail.com>
2018-09-30 03:02:38 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <Text>Hello world!</Text>
+        );
+    }
+}
