afd40228b726359028ee766a354d6fca93fcc479
Arne Van de Bemdt <a.vandebemdt@icloud.com>
2018-10-09 11:32:31 +0200

    fixed mysql_port bug

--- models/sequelize-connection.js
+++ models/sequelize-connection.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
-    port: 3306,
+    port: process.env.MYSQL_PORT,
     dialect: 'mysql',
     operatorsAliases: false,
     pool: {
