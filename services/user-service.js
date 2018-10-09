3f7a601e7bdfe3f6c3d19734be728b55dd124149
Tom Daniel <tomdaniel.it@gmail.com>
2018-10-09 13:55:58 +0200

    Update user-service.js

--- services/user-service.js
+++ services/user-service.js
@@ -2,8 +2,6 @@ const User = require("../models/user");
 const bcrypt = require("../auth/bcrypt");
 
 function createUser(firstname, lastname, birthday, username, password, passwordConf) {
-    // TODO: CHECK PASSWD
-
     return new Promise((resolve, reject) =>{
         if (password !== passwordConf) {
             return reject(new Error("Passwords don't match."));
