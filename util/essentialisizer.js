b42358e41bec488d04625b8cfe8fe93d5dcdc4a1
tomdaniel-it <tomdaniel.it@gmail.com>
2018-10-10 19:26:24 +0200

    Added essentialisizer

---
+++ util/essentialisizer.js
@@ -0,0 +1,13 @@
+const clone = obj => JSON.parse(JSON.stringify(obj)); // Also removes all functions
+
+function essentializyGroup(group) {
+    const r = clone(group);
+    return { id: r.id, name: r.name, description: r.description, color: r.color, creator: r.creator_username };
+}
+
+function essentializyUser(user) {
+    const r = clone(user);
+    return { firstname: r.firstname, lastname: r.lastname, username: r.username, birthday: r.birthday };
+}
+
+module.exports = { essentializyGroup, essentializyUser };
