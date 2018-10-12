482acd0d951718fb33ab83ba0cce9957b1192b95
tomdaniel-it <tomdaniel.it@gmail.com>
2018-10-12 19:39:07 +0200

    Added getting user info functionality

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userService = require("../services/user-service");
+const middleware = require('../middleware');
+const essentialisizer = require('../util/essentialisizer');
 
 router.post('/', (req, res) => {
     userService.createUser(req.body.firstname, req.body.lastname, req.body.birthday, req.body.username, req.body.password, req.body.passwordConf)
@@ -9,6 +11,16 @@ router.post('/', (req, res) => {
     })
     .catch((error) => {
         res.status(400).send({error: error.message});
+    });
+});
+
+router.get('/:username', middleware.auth.loggedIn, (req, res) => {
+    userService.getUser(req.user, req.params.username)
+    .then(user => {
+        res.send(essentialisizer.essentializyUser(user));
+    })
+    .catch(error => {
+        res.status(400).send({ error: error.message });
     });
 });
 
--- services/user-service.js
+++ services/user-service.js
@@ -50,4 +50,28 @@ function loginUser(username, password) {
     });
 }
 
-module.exports = { createUser, loginUser };
+function getUser(currentUser, username) {
+    return new Promise((resolve, reject) => {
+        if (currentUser.username === username) return resolve(currentUser);
+        let tmpUser;
+        User.User.findByPrimary(username)
+        .then(user => {
+            if (!user) return Promise.reject(new Error('That user does not exist.'));
+            tmpUser = user;
+            return Promise.all([ currentUser.getGroups(), user.getGroups() ]); // Waits for both promises to succeed and returns both return values in an array [1, 2]
+        })
+        .then(results => {
+            let result = results[0].concat(results[1]).map(el => el.id); // Concat joins the two arrays together
+            // .map transforms every element { id: x, name: y, ... } to x
+
+            if (result.length !== new Set(result).size) { // Transforming an array to a set removes all doubles, meaning they're in a joined group if the sizes are not the same
+                resolve(tmpUser);
+            } else {
+                return Promise.reject(new Error('You\'re not allowed to get info about that user.'));
+            }
+        })
+        .catch(reject); // == `.catch(error => reject(error));` == `.catch(error => { reject(error); });`
+    });
+}
+
+module.exports = { createUser, loginUser, getUser };
