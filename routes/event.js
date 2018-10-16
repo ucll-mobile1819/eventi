019c078b5e1d0f509e48ad9c653c924af8f82d66
tomdaniel-it <tomdaniel.it@gmail.com>
2018-10-16 16:49:18 +0200

    Added more checks and linked events to users and groups

--- routes/event.js
+++ routes/event.js
@@ -2,34 +2,28 @@ const express = require('express');
 const router = express.Router();
 const eventService = require("../services/event-service");
 
-router.post('/', (req, res) => {
-    eventService.createEvent(req.body.name,req.body.description,req.body.startdate,req.body.enddate,req.body.locationname,req.body.zipcode,req.body.city,req.body.adress,req.body.housenumber)
+router.post('/', (req, res, next) => {
+    eventService.createEvent(req.user, req.body.groupId, req.body.name,req.body.description,req.body.startDate,req.body.endDate,req.body.locationName,req.body.zipcode,req.body.city,req.body.adress,req.body.housenumber)
     .then(result => {
         res.send(result);
     })
-    .catch((error) => {
-        res.status(400).send({error: error.message});
-    });
+    .catch(next);
 });
 
-router.get('/',(req,res) =>{
-    eventService.housenumber()
+router.get('/', (req, res, next) =>{
+    eventService.getAllEvents(req.user)
     .then(result => {
         res.send(result);
     })
-    .catch((error) => {
-        res.status(400).send({error: error.message});
-    });
+    .catch(next);
 });
 
-router.get('/:id',(req,res) =>{
-    eventService.getAllEvents()
+router.get('/:id', (req, res, next) =>{
+    eventService.getEvent(req.user, req.params.id)
     .then(result => {
         res.send(result);
     })
-    .catch((error) => {
-        res.status(400).send({error: error.message});
-    });
+    .catch(next);
 });
 
 
--- services/event-service.js
+++ services/event-service.js
@@ -1,47 +1,79 @@
 const Event = require("../models/event");
-const auth = require('../auth/auth');
+const Group = require('../models/group');
 const Sequelize = require('sequelize');
 const op = Sequelize.Op;
-function createEvent(name,description,start_date,end_date,location_name,adress,zipcode,city,housenumber){
 
+function createEvent(currentUser, groupId, name,description,startDate,endDate,locationName,adress,zipcode,city,housenumber){
     return new Promise((resolve, reject) =>{
-        
-          Event.Event.create({
-              name,
-              description,
-              start_date,
-              end_date,
-              location_name,
-              zipcode,
-              city,
-              adress,
-              housenumber
-            }).then((res) => {
-            resolve(res);
-        }).catch(() => {
-            reject(new Error("Something went wrong with creating an event"));
-        })
-        
-
-    });
-}
-
-function getAllEvents(){
-    //Change date for timezone?
-
-    return new Promise((res,rej) => {
-        Event.Event.findAll({
-            where: {
-                // usename: usename,
-                enddate:{
-                    [op.gt]: Date.now,
-                }
+        let tmpGroup;
+        let tmpEvent;
+        Group.Group.findById(groupId)
+        .then(group => {
+            if (!group) {
+                return Promise.reject(new Error('This group does not exist.'));
             }
-        }).then(results =>{
-            res(results);
-        }).catch(rej);
+            tmpGroup = group;
+            return group.getUsers({ where: { username: currentUser.username } });
+        })
+        .then(users => {
+            if (users.length === 0) {
+                return Promise.reject(new Error('You do not belong to this group.'));
+            }
+            return Event.Event.create({
+                name,
+                description,
+                start_date: startDate,
+                end_date: endDate,
+                location_name: locationName,
+                zipcode,
+                city,
+                adress,
+                housenumber
+            });
+        }).then((event) => {
+            tmpEvent = event;
+            return Promise.all([ tmpGroup.addEvent(event), event.setCreator(currentUser) ]);
+        })
+        .then(() => resolve(tmpEvent))
+        .catch(() => {
+            reject(new Error("Something went wrong with creating an event"));
+        });
     });
-    
 }
 
-module.exports = {createEvent,getAllEvents}
+function getAllEvents(currentUser) {
+    return new Promise((res, rej) => {
+        currentUser.getGroups()
+        .then(groups => {
+            let promises = [];
+            groups.forEach(group => promises.push(group.getEvents({
+                where: { end_date: { [op.gt]: Date.now } }
+            })));
+            return Promise.all(promises);
+        })
+        .then(results => {
+            results = [].concat.apply([], results);
+            res(results);
+        });
+    });
+}
+
+function getEvent(currentUser, eventId) {
+    return new Promise((res, rej) => {
+        let tmpEvent;
+        Event.Event.findById(eventId)
+        .then(event => {
+            if (!event) return Promise.reject(new Error('This event does not exist.'));
+            tmpEvent = event;
+            return event.getGroup();
+        })
+        .then(group => group.getUsers({ where: { username: currentUser.username } }))
+        .then(users => {
+            if (users.length === 0) return Promise.reject(new Error('You do not belong to the group of this event.'));
+            res(tmpEvent);
+        })
+        .catch(rej);
+    });
+}
+
+module.exports = { createEvent, getAllEvents, getEvent };
