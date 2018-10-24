ff346ff62d3d029a2248be7f7988cf18ea833a2b
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-10-24 16:24:08 +0200

    added routes for comments

---
+++ routes/comment.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const commentService = require("../services/comment-service");
+const middleware = require('../middleware');
+const essentialisizer = require('../util/essentialisizer');
+
+// get all comments of a user isn't needed?
+
+// get a single comment isn't needed?
+
+router.get('/event/:eventId', middleware.auth.loggedIn, (req, res, next) => {
+    commentService.getCommentsOfEvent(req.user, req.params.eventId)
+    .then(comments => res.send(comments))
+    .catch(next);
+})
+
+router.post('/', middleware.auth.loggedIn, (req, res, next) => {
+    commentService.createComment(req.user, req.body.eventId, req.body.content)
+    .then(result => {
+        res.send(essentialisizer.essentializyComment(result)); // is essentializy necessary?
+    })
+    .catch(next);
+})
+
+router.put('/:id', middleware.auth.loggedIn, (req, res, next) => {
+    commentService.updateComment(req.user, req.params.id, req.body.content)
+    .then(() => res.send())
+    .catch(next);
+})
+
+router.delete('/:id', middleware.auth.loggedIn, (req, res, next) => {
+    commentService.deleteComment(req.user, req.params.id)
+    .then(() => res.send())
+    .catch(next);
+})
+
+module.exports = router;
