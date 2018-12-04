29c84239513f0e2bee9b3003d1ce6a676dbe9dad
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-12-04 13:59:45 +0100

    added an initial register screen

---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import { customStyles } from '../styles/customStyles';
+
+export class RegisterScreen extends Component {
+    render() {
+        return (
+            <View style={{alignItems: 'center', flex: 1 }}>
+                <Text style={customStyles.bigTitle}>Eventi</Text>
+                <Text style={customStyles.smallTitle}>Register a new account</Text>
+                <TextInput style={customStyles.inputField} placeholder="First name"/>
+                <TextInput style={customStyles.inputField} placeholder="Last name"/>
+                <TextInput style={customStyles.inputField} placeholder="Username"/>
+                <TextInput style={customStyles.inputField} placeholder="Email"/>
+                <TextInput style={customStyles.inputField} placeholder="Birth date"/>
+                <TextInput style={customStyles.inputField} placeholder="Username"/>
+                <TextInput style={customStyles.inputField} secureTextEntry={true} placeholder="Password" />
+                <TextInput style={customStyles.inputField} secureTextEntry={true} placeholder="Repeat password" />
+                <Text>Go back to login</Text>
+            </View>
+        );
+    }
+}
