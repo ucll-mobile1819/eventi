5a4e1b876adcd36c6f81d8c9ac708ce6c61a385c
Senne <Senne.vandegaer2@gmail.com>
2018-12-24 11:20:39 +0100

    loginscreen updated ui

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import loginregisterStyles from '../styles/loginregister';
+import { StyleSheet } from 'react-native';
+import { Text,Button, Container, Content, Item,Input, Label, Form, Header } from 'native-base';
 import { isAuthenticated, login } from '../auth';
 import { NavigationEvents } from 'react-navigation';
+import { Fonts } from '../utils/Fonts';
 
 export default class LoginScreen extends Component {
     constructor(props) {
@@ -31,31 +32,56 @@ export default class LoginScreen extends Component {
 
     render() {
         return (
-            <View style={{alignItems: 'center', flex: 1 }}>
+            <Container>
+                <Content>
                 <NavigationEvents onWillFocus={() => this.onNavWillFocus()} />
-                <Text style={loginregisterStyles.bigTitle}>Eventi</Text>
-                <TextInput
-                    style={loginregisterStyles.inputField}
-                    placeholder="Username"
+                <Text style={styles.title}>Eventi</Text>
+                <Form>
+                <Item floatingLabel>
+                    <Label><Text>Username</Text></Label>
+                  <Input 
                     value={this.state.username}
                     onChangeText={username => this.setState({ username })}
-                />
-                <TextInput
-                    style={loginregisterStyles.inputField}
+                    />
+                 </Item>
+                
+                 <Item floatingLabel>
+                 <Label><Text>Password</Text></Label>
+                  <Input 
                     secureTextEntry={true}
-                    placeholder="Password"
                     value={this.state.password}
                     onChangeText={password => this.setState({ password })}
-                />
+                    />
+                 </Item>
+                
                 <Button
-                    title="Login"
-                    onPress={() => this.login()}
-                />
+                    style={{marginLeft:10, marginTop:20,marginRight:10}}
+                    
+                    block
+                    onPress={() => this.login()}>
+                    <Text>Login</Text>
+                </Button>
+                
+                </Form>
                 <Text 
                     onPress={() => this.props.navigation.navigate('Register')}
-                    style={{ color: 'darkblue', marginTop: 10 }}
+                    style={{ color: 'darkblue', marginTop: 10,alignSelf:"center" }}
                 >Not a member yet? Register here.</Text>
-            </View>
+                </Content>
+            </Container>
         );
     }
 }
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: Fonts.OpenSansBold,
+    fontSize: 40,
+    color: 'black',
+    alignSelf: "center"
+    
+  },
+  container:{
+    // alignItems: "center",
+
+   },
+});
