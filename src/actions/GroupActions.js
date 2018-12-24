682d51019c386e7b3c80bae00feee936b6639cc4
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-12-24 15:16:02 +0100

    added actions for fetching a single group

--- src/actions/GroupActions.js
+++ src/actions/GroupActions.js
@@ -3,6 +3,8 @@ import * as groupAPI from '../network/group';
 
 export const FETCH_GROUPS_BEGIN = 'FETCH_GROUPS_BEGIN';
 export const FETCH_GROUPS_SUCCESS = 'FETCH_GROUPS_SUCCESS';
+export const FETCH_GROUP_BEGIN = 'FETCH_GROUP_BEGIN';
+export const FETCH_GROUP_SUCCESS = 'FETCH_GROUP_SUCCESS';
 
 export const fetchGroups = () => dispatch => {
     dispatch(fetchGroupsBegin());
@@ -18,4 +20,20 @@ export const fetchGroupsBegin = () => ({
 export const fetchGroupsSuccess = groups => ({
     type: FETCH_GROUPS_SUCCESS,
     payload: { groups },
+});
+
+export const fetchGroup = groupId => dispatch => {
+    dispatch(fetchGroupBegin());
+    groupAPI.getGroup(groupId)
+    .then(group => dispatch(fetchGroupSuccess(group)))
+    .catch(error => dispatch(fetchFailure(error)));
+};
+
+export const fetchGroupBegin = () => ({
+    type: FETCH_GROUP_BEGIN
+});
+
+export const fetchGroupSuccess = group => ({
+    type: FETCH_GROUP_SUCCESS,
+    payload: { group }
 });
