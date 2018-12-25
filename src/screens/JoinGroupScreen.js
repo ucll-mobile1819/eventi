a2325c336d5e21f98cca8c5e8e6e4d2314da9d7c
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-12-25 19:05:35 +0100

    added a form to JoinGroupScreen to enter an invitecode

--- src/screens/JoinGroupScreen.js
+++ src/screens/JoinGroupScreen.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput, Button, View } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import AuthenticatedComponent from '../components/AuthenticatedComponent';
+import groupStyles from '../styles/groupStyles';
+import loginregisterStyles from '../styles/loginregister';
 
 class JoinGroupScreen extends React.Component {
     render() {
         return (
             <AuthenticatedComponent navigate={this.props.navigation.navigate}>
-                <Text>JoinGroupScreen</Text>
+                <View style={{ flex: 1, padding: 20 }} >
+                    <Text>Enter an invitecode here to join a group.</Text>
+                    {/* {this.isFieldInError('invitecode') && <Text style={loginregisterStyles.inputError}>{this.getErrorsInField('invitecode')[0]}</Text>} */}
+                    <TextInput
+                        style={groupStyles.inputField}
+                        placeholder="Invitecode"
+                        // value={this.state.inviteCode}
+                        onChangeText={invitecode => this.setState({ invitecode })}
+                    />
+                    <Button
+                        title="Join group"
+                        onPress={() => this.joinGroup()}
+                    />
+                </View>
             </AuthenticatedComponent>
         );
     }
