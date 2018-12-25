593dbd6da768599422fc0a650196ff70d8e1afa1
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-12-25 12:31:16 +0100

    added memberCount to groups fetched with fetchGroups action

--- src/actions/GroupActions.js
+++ src/actions/GroupActions.js
@@ -8,9 +8,25 @@ export const FETCH_GROUP_SUCCESS = 'FETCH_GROUP_SUCCESS';
 
 export const fetchGroups = () => dispatch => {
     dispatch(fetchGroupsBegin());
+    let newGroups = [];
+
     groupAPI.getGroups()
-    .then(groups => dispatch(fetchGroupsSuccess(groups)))
-    .catch(error => dispatch(fetchFailure(error)));
+        .then(groups => {
+            // get a list of all promises that are going to get the memberCount of each group
+            let promises = [];       
+            groups.forEach(group => promises.push(groupAPI.getMemberCountOfGroup(group.id)));
+
+            // run all promises and put their values in a new array                
+            return Promise.all(promises)
+            .then(counts => {
+                groups.forEach((group, index) => {
+                    groups[index].memberCount = counts[index].count;
+                })
+                return groups;
+            });
+        })
+        .then(groups => dispatch(fetchGroupsSuccess(groups)))
+        .catch(error => dispatch(fetchFailure(error)));
 };
 
 export const fetchGroupsBegin = () => ({
@@ -25,8 +41,8 @@ export const fetchGroupsSuccess = groups => ({
 export const fetchGroup = groupId => dispatch => {
     dispatch(fetchGroupBegin());
     groupAPI.getGroup(groupId)
-    .then(group => dispatch(fetchGroupSuccess(group)))
-    .catch(error => dispatch(fetchFailure(error)));
+        .then(group => dispatch(fetchGroupSuccess(group)))
+        .catch(error => dispatch(fetchFailure(error)));
 };
 
 export const fetchGroupBegin = () => ({
