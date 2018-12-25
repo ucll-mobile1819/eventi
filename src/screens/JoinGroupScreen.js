4409f7c353c807446b3ceb7ea6e709441b50dffc
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2018-12-25 19:15:53 +0100

    added placeholder functionality for posting form and removed state-related code

--- src/screens/JoinGroupScreen.js
+++ src/screens/JoinGroupScreen.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { Text, TextInput, Button, View } from 'react-native';
+import { Text, TextInput, Button, View, Alert } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import AuthenticatedComponent from '../components/AuthenticatedComponent';
 import groupStyles from '../styles/groupStyles';
 import loginregisterStyles from '../styles/loginregister';
+import ValidationComponent from '../components/ValidationComponent';
 
-class JoinGroupScreen extends React.Component {
+export default class JoinGroupScreen extends ValidationComponent {
+    constructor(props) {
+        super(props);
+        this.state = this.getClearedState();
+    }
+
+    getClearedState() {
+        return { invitecode: '' };
+    }
+
+    async joinGroup() {
+        if (!this.validateForm()) return;
+
+        Alert.alert('Join group');
+    }
+
+    validateForm() {
+        if (!this.validate({
+            invitecode: { name: 'Invitecode', required: true, invitecode: true },
+        })) {
+            return false;
+        }
+        return true;
+    }
+
     render() {
         return (
             <AuthenticatedComponent navigate={this.props.navigation.navigate}>
                 <View style={{ flex: 1, padding: 20 }} >
                     <Text>Enter an invitecode here to join a group.</Text>
-                    {/* {this.isFieldInError('invitecode') && <Text style={loginregisterStyles.inputError}>{this.getErrorsInField('invitecode')[0]}</Text>} */}
+                    {this.isFieldInError('invitecode') && <Text style={loginregisterStyles.inputError}>{this.getErrorsInField('invitecode')[0]}</Text>}
                     <TextInput
                         style={groupStyles.inputField}
                         placeholder="Invitecode"
-                        // value={this.state.inviteCode}
+                        value={this.state.inviteCode}
                         onChangeText={invitecode => this.setState({ invitecode })}
                     />
                     <Button
@@ -28,15 +53,3 @@ class JoinGroupScreen extends React.Component {
         );
     }
 }
-
-const mapStateToProps = state => {
-    return {
-    };
-};
-
-const mapDispatchToProps = dispatch => (
-    bindActionCreators({
-    }, dispatch)
-);
-
-export default connect(mapStateToProps, mapDispatchToProps)(JoinGroupScreen);
