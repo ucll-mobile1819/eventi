198667e27d6f9ea962df9dd06f0646f84c962af2
Tom Daniel <tomdaniel.it@gmail.com>
2019-01-03 14:34:50 +0100

    Added / route to user for getting own user information

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const userService = require("../services/user-service");
 const middleware = require('../middleware');
 
-router.post('/', (req, res, next) => {
-    userService.createUser(req.body.firstname, req.body.lastname, req.body.birthday, req.body.username, req.body.password, req.body.passwordConf)
-    .then(() => {
-        res.send();
-    })
+router.get('/', middleware.auth.loggedIn, (req, res, next) => {
+    userService.getUser(req.user, req.user.username)
+    .then(user => res.send(user))
     .catch(next);
 });
 
@@ -15,6 +13,14 @@ router.get('/:username', middleware.auth.loggedIn, (req, res, next) => {
     userService.getUser(req.user, req.params.username)
     .then(user => {
         res.send(user);
+    })
+    .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+    userService.createUser(req.body.firstname, req.body.lastname, req.body.birthday, req.body.username, req.body.password, req.body.passwordConf)
+    .then(() => {
+        res.send();
     })
     .catch(next);
 });
