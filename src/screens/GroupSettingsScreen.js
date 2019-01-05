aa04810764691558591d82227726882a418fd85a
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2019-01-05 17:06:30 +0100

    added color of group to its settings screen

--- src/screens/GroupSettingsScreen.js
+++ src/screens/GroupSettingsScreen.js
@@ -3,16 +3,30 @@ import { Text, Button } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import AuthenticatedComponent from '../components/AuthenticatedComponent';
+import { fetchGroup } from '../actions/GroupActions';
 
 class GroupSettingsScreen extends React.Component {
     static navigationOptions = obj => obj.navigation.state.params;
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            showActivityIndicator: true
+        };
+    }
+
     onLoad() {
-        this.props.navigation.setParams({
-            title: this.props.navigation.state.params.id.toString(),
-            customHeaderBackgroundColor: '#f44242',
-            headerTintColor: 'white', // Back arrow color
-            headerTitleStyle: { color: 'white' }, // Title color
+        this.props.fetchGroup(this.props.navigation.state.params.id)
+        .then(() => {
+            this.setState({ showActivityIndicator: false });
+            if (this.props.error) return;
+            this.props.navigation.setParams({
+                // title: this.props.navigation.state.params.id.toString(),
+                title: 'Settings',
+                customHeaderBackgroundColor: this.props.group.color,
+                headerTintColor: 'white', // Back arrow color
+                headerTitleStyle: { color: 'white' }, // Title color
+            });
         });
     }
 
@@ -27,11 +41,14 @@ class GroupSettingsScreen extends React.Component {
 
 const mapStateToProps = state => {
     return {
+        group: state.group.group,
+        error: state.group.error
     };
 };
 
 const mapDispatchToProps = dispatch => (
     bindActionCreators({
+        fetchGroup
     }, dispatch)
 );
 
