a42efcdf3c517d83f1c98c218f6766d3fa711d01
Tom Daniel <tomdaniel.it@gmail.com>
2019-01-06 00:35:24 +0100

    Added configure mode for PollTableComponent with add & remove option

--- src/components/PollTableComponent.js
+++ src/components/PollTableComponent.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Table, Row, TableWrapper, Cell } from 'react-native-table-component';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import DatePicker from 'react-native-datepicker';
+
+const months = [ "jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+const weekdays = [ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ];
 
 const red = '#DD1111';
 const green = '#11DD52';
 const grey = '#a8aeb7';
 
-// <FontAwesomeIcon name="check" size={15} color={status == 'Going'? green : grey}/>
-// <FontAwesomeIcon name="close" size={15} color={status == 'Going'? green : grey}/>
+/*
+PROPS:
+    mode: string (required)
+    pollDateVotes: array (required)
+    pollDates: array
+    votesUpdated: function, i.e.: (newVotes) => { ... }
+    showAmountOfVotes: boolean, only works in configure mode, needs to be true for EditEventScreen ! ! !
+    selectable: boolean, allows user to select a polldate
+    fixed: function which returns boolean, i.e.: () => { ... return true||false; }
+    newPollDateAdded: function, i.e.: (newPollDate) => { ... }
+    pollDateRemoved: function, i.e.: (pollDateId) => { ... }
+*/
 
 export default class PollTableComponent extends Component {
     constructor(props) {
@@ -35,76 +50,221 @@ export default class PollTableComponent extends Component {
             tableHead = [
                 'Start',
                 'End',
-                <FontAwesomeIcon name="check" size={20} color={green} style={{ alignSelf: 'center' }} />,
+                <FontAwesomeIcon name="check" size={25} color={green} style={{ alignSelf: 'center' }} />,
                 ''
             ];
             tableData = [
 
             ];
         } else {
-            tableHead = [ 'Start', 'End', '' ];
+            tableHead = this.props.showAmountOfVotes ?
+                [ 'Start', 'End', <FontAwesomeIcon name="check" size={25} color={green} style={{ alignSelf: 'center' }} />, '' ] :
+                [ 'Start', 'End', '' ];
             tableData = [];
         }
+        this.tmpDatePickerDates = { startTime: null, endTime: null };
         this.state = {
             tableHead,
             tableData,
-            pollDates: this.props.pollDates || [],
             newPollDate: null,
-            pollDateVotes,
+            lastInterval: 0,
         };
     }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-                    <Row data={this.state.tableHead} flexArr={[3, 3, 1, 2]} style={styles.head} textStyle={styles.text}/>
-                    {this.props.mode === 'overview' ? this.renderRowsOverview() : this.renderRowsConfigure()}
-                </Table>
-            </View>
-        );
+    getPollDates() {
+        return this.props.pollDates || [];
     }
 
     formatDate(d) {
         if (!(d instanceof Date)) return null;
         let dbl = num => num.toString().length === 1 ? '0' + num.toString() : num.toString();
-        return `${dbl(d.getDate())}-${dbl(d.getMonth()+1)}-${d.getFullYear()} ${dbl(d.getHours())}:${dbl(d.getMinutes())}`;
+        let weekday = date => weekdays[(date.getDay()||7)-1];
+        return `${weekday(d)} ${d.getDate()} ${months[d.getMonth()]} ${dbl(d.getHours())}:${dbl(d.getMinutes())}`;
+    }
+
+    sortPollDates(pollDates) {
+        return pollDates.sort((a, b) => (a.startTime.getTime() - b.startTime.getTime()) || a.id - b.id);
+    }
+
+    voteOnPollDate(id, available) {
+        if (!(this.props.votesUpdated instanceof Function)) return;
+        let votes = [...this.props.pollDateVotes];
+        if (available && !votes.includes(id)) votes.push(id);
+        if (!available && votes.includes(id)) votes.splice(votes.indexOf(id), 1);
+        this.props.votesUpdated(votes);
+    }
+
+    addNewPollDate() {
+        this.setState({
+            newPollDate: {
+                startTime: null,
+                endTime: null,
+            },
+        });
+    }
+
+    isNewPollDateValid() {
+        let item = this.state.newPollDate;
+        return item.startTime instanceof Date && 
+            item.endTime instanceof Date &&
+            item.endTime.getTime() - item.startTime.getTime() > 0;
+    }
+
+    saveNewPollDate() {
+        if (!this.isNewPollDateValid()) return;
+        if (this.props.newPollDateAdded instanceof Function) this.props.newPollDateAdded(this.state.newPollDate);
+        this.setState({
+            lastInterval: this.state.newPollDate.endTime.getTime() - this.state.newPollDate.startTime.getTime(),
+            newPollDate: null,
+        });
+    }
+
+    removePollDate(id) {
+        if (this.props.pollDateRemoved instanceof Function) this.props.pollDateRemoved(id);
+    }
+
+    datePickerConvertDate(dateCat) {
+        return this.tmpDatePickerDates[dateCat];
+    }
+
+    formatDateForDatePicker(d, dateCat) {
+        this.tmpDatePickerDates[dateCat] = d;
+        if (!(d instanceof Date)) return null;
+        let dbl = num => num.toString().length === 1 ? '0' + num.toString() : num.toString();
+        let weekday = date => weekdays[(date.getDay()||7)-1];
+        return `${weekday(d)} ${d.getDate()} ${months[d.getMonth()]} ${dbl(d.getHours())}:${dbl(d.getMinutes())}`;
+    }
+
+    onDatePickerChange(dateCat) {
+        let times = {
+            endTime: this.state.newPollDate.endTime,
+            startTime: this.state.newPollDate.startTime,
+        };
+        times[dateCat] = this.datePickerConvertDate(dateCat);
+        if (this.state.lastInterval !== 0 && dateCat === 'startTime') {
+            times.endTime = new Date(times.startTime.getTime() + this.state.lastInterval);
+        }
+        this.setState({
+            newPollDate: times
+        });
+    }
+
+    render() {
+        let { mode, showAmountOfVotes } = this.props;
+        return (
+            <View style={styles.container}>
+                <Table borderStyle={styles.border}>
+                    <Row data={this.state.tableHead} flexArr={mode == 'overview' ? [3, 3, 1, 2] : showAmountOfVotes ? [7, 7, 2, 2] : [4, 4, 1] } style={styles.head} textStyle={styles.textHeader}/>
+                    {mode === 'overview' ? this.renderRowsOverview() : this.renderRowsConfigure()}
+                </Table>
+                {mode === 'configure' ? this.renderPlusButton() : <></>}
+            </View>
+        );
     }
 
     renderRowsOverview() {
-        const { pollDates, newPollDate, pollDateVotes } = this.state;
+        let pollDates = this.getPollDates();
+        let pollDateVotes = this.props.pollDateVotes;
         return (
             <>
-                {pollDates.map((item, index) => 
-                    <TableWrapper key={index} flexArr={[3, 3, 1, 2]} style={styles.row}>
+                {this.sortPollDates(pollDates).map((item, index) => 
+                    <TableWrapper key={index} flexArr={[3, 3, 1, 2]} style={styles.row} borderStyle={styles.border}>
                         <Cell key={0} flex={3} data={this.formatDate(item.startTime)} textStyle={styles.date}/>
-                        <Cell key={1} flex={3} data={this.formatDate(item.startTime)} textStyle={styles.date}/>
+                        <Cell key={1} flex={3} data={this.formatDate(item.endTime)} textStyle={styles.date}/>
                         <Cell key={2} flex={1} data={item.votes} textStyle={styles.votes}/>
                         <Cell key={3} flex={2} data={
-                            <View>
-                                <FontAwesomeIcon name="check" size={20} color={pollDateVotes.includes(item.id) ? green : grey}/>
-                                <FontAwesomeIcon name="close" size={20} color={pollDateVotes.includes(item.id) ? grey : red}/>
+                            <View style={styles.buttonContainer}>
+                                <View style={styles.buttons}>
+                                    <TouchableWithoutFeedback onPress={() => this.voteOnPollDate(item.id, true)}>
+                                        <FontAwesomeIcon name="check" size={25} color={pollDateVotes.includes(item.id) ? green : grey}/>
+                                    </TouchableWithoutFeedback>
+                                </View>
+                                <View style={styles.buttons}>
+                                    <TouchableWithoutFeedback onPress={() => this.voteOnPollDate(item.id, false)}>
+                                        <FontAwesomeIcon name="close" size={25} color={pollDateVotes.includes(item.id) ? grey : red}/>
+                                    </TouchableWithoutFeedback>
+                                </View>
                             </View>
-                        } textStyle={styles.buttons}/>
+                        }/>
                     </TableWrapper>
                 )}
-                {newPollDate !== null &&
-                    <TableWrapper key={pollDates.length} style={styles.row}>
-                        <Cell key={0} data={"Test1"} />
-                        <Cell key={1} data={"Test2"} />
-                        <Cell key={2} data={"Test3"} />
-                        <Cell key={3} data={"Test4"} />
+            </>
+        );
+    }
+
+    renderRowsConfigure() {
+        let pollDates = this.getPollDates();
+        let newPollDate = this.state.newPollDate;
+        let { pollDateVotes, showAmountOfVotes } = this.props;
+        return (
+            <>
+                {this.sortPollDates(pollDates).map((item, index) => 
+                    <TableWrapper key={index} flexArr={showAmountOfVotes ? [7, 7, 2, 2] : [4, 4, 1]} style={styles.row} borderStyle={styles.border}>
+                        <Cell key={0} flex={showAmountOfVotes ? 7 : 4} data={this.formatDate(item.startTime)} textStyle={styles.date}/>
+                        <Cell key={1} flex={showAmountOfVotes ? 7 : 4} data={this.formatDate(item.endTime)} textStyle={styles.date}/>
+                        {showAmountOfVotes ? <Cell key={2} flex={2} data={item.votes} textStyle={styles.votes}/> : <></>}
+                        <Cell key={showAmountOfVotes ? 3 : 2} flex={showAmountOfVotes ? 2 : 1} data={
+                            <View style={styles.buttonContainer}>
+                                <TouchableWithoutFeedback onPress={() => this.removePollDate(item.id)}>
+                                    <MaterialIcon name="delete" size={25} color={grey}/>
+                                </TouchableWithoutFeedback>
+                            </View>
+                        }/>
+                    </TableWrapper>
+                )}
+                {newPollDate != null &&
+                    <TableWrapper key={pollDates.length} flexArr={showAmountOfVotes ? [7, 7, 2, 2] : [4, 4, 1]} style={styles.row} borderStyle={styles.border}>
+                        <Cell key={0} flex={showAmountOfVotes ? 7 : 4} data={
+                            <DatePicker
+                                placeholder="Enter start time"
+                                getDateStr={date => this.formatDateForDatePicker(date, 'startTime')}
+                                minDate={new Date()}
+                                maxDate={new Date(Date.now() + 500*365*24*60*60*1000)}
+                                confirmBtnText="Confirm"
+                                cancelBtnText="Cancel"
+                                showIcon={false}
+                                onDateChange={() => this.onDatePickerChange('startTime')}
+                                customStyles={{ dateInput: { borderWidth: 0, alignItems: 'flex-start', paddingLeft: 6 }}}
+                                date={newPollDate.startTime}
+                                mode="datetime"
+                            />
+                        }/>
+                        <Cell key={1} flex={showAmountOfVotes ? 7 : 4} data={
+                            <DatePicker
+                                placeholder="Enter end time"
+                                getDateStr={date => this.formatDateForDatePicker(date, 'endTime')}
+                                minDate={new Date()}
+                                maxDate={new Date(Date.now() + 500*365*24*60*60*1000)}
+                                confirmBtnText="Confirm"
+                                cancelBtnText="Cancel"
+                                showIcon={false}
+                                onDateChange={ () => this.setState({ newPollDate: { ...newPollDate, endTime: this.datePickerConvertDate('endTime') }})}
+                                customStyles={{ dateInput: { borderWidth: 0, alignItems: 'flex-start', paddingLeft: 6 }}}
+                                date={newPollDate.endTime}
+                                mode="datetime"
+                            />
+                        }/>
+                        {showAmountOfVotes ? <Cell key={2} flex={2} data={''}/> : <></>}
+                        <Cell key={showAmountOfVotes ? 3 : 2} flex={showAmountOfVotes ? 2 : 1} data={
+                            <View style={styles.buttonContainer}>
+                                <TouchableWithoutFeedback onPress={() => this.saveNewPollDate()}>
+                                    <FontAwesomeIcon name="check" size={25} color={this.isNewPollDateValid() ? green : grey}/>
+                                </TouchableWithoutFeedback>
+                            </View>
+                        }/>
                     </TableWrapper>
                 }
             </>
         );
     }
 
-    renderRowsConfigure() {
+    renderPlusButton() {
         return (
-            <>
-
-            </>
+            <TouchableWithoutFeedback onPress={() => this.addNewPollDate()}>
+                <View style={styles.addButton}>
+                    <Text>+</Text>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -115,7 +275,7 @@ const styles = StyleSheet.create({
     },
     head: {
         height: 40,
-        backgroundColor: '#f1f8ff',
+        backgroundColor: '#002984',
     },
     date: {
         margin: 6,
@@ -124,12 +284,40 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
     },
     buttons: {
-        alignSelf: 'center',
+        alignContent: 'center',
+        margin: 5,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
         alignContent: 'center',
         alignItems: 'center',
+        alignSelf: 'center',
     },
     row: {
         flexDirection: 'row',
-        backgroundColor: '#FFF1C1'
+        backgroundColor: 'white',
+    },
+    text: {
+        marginLeft: 6,
+    },
+    textHeader: {
+        marginLeft: 6,
+        color: 'white',
+    },
+    border: {
+        borderWidth: 1,
+        borderColor: '#021957'
+    },
+    addButton: {
+        borderBottomWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderColor: '#021957',
+        width: 30,
+        height: 30,
+        backgroundColor: 'white',
+        alignContent: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
 });
