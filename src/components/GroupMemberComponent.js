13db91de531cedcc3153b3156121583cd72a9511
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2019-01-06 20:49:00 +0100

    implemented banning functionality and some styling

--- src/components/GroupMemberComponent.js
+++ src/components/GroupMemberComponent.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import { Text, View, Button, Alert } from 'react-native';
-
+import { postBanUser } from '../network/group';
+import Snackbar from 'react-native-snackbar';
 
 export default class GroupMemberComponent extends React.Component {
     askBanMember() {
         Alert.alert(
             'Ban member',
-            'Are you sure that you want to ban this user?',
+            'Are you sure you want to ban ' + this.props.member.username + '?',
             [
-                { text: 'Delete', onPress: () => this.banMember() },
+                { text: 'Ban', onPress: () => this.banMember() },
                 { text: 'Cancel', style: 'cancel' }
             ],
             { cancelable: false }
         );
     }
 
-    banMember() {
-        Alert.alert("BANHAMMER");
+    async banMember() {
+        let response = await postBanUser(this.props.groupId, this.props.member.username, true);
+
+        if (response !== false) {
+            Snackbar.show({
+                title: this.props.member.username + ' was banned',
+                duration: Snackbar.LENGTH_LONG,
+                action: {
+                    title: 'CLOSE',
+                    color: 'green',
+                    onPress: () => Snackbar.dismiss(),
+                }
+            });
+            // TODO refresh memberlist of group
+        }
     }
 
     render() {
         return (
-            <View style={{ flexDirection: 'row' }}>
-                <Text>{this.props.member.username}</Text>
-                <Text>({this.props.member.firstname} {this.props.member.lastname})</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+                <Text>{this.props.member.firstname} {this.props.member.lastname}</Text>
+                <Text style={{ flex: 1, marginLeft: 10 }}>({this.props.member.username})</Text>
                 <Button title="Ban" onPress={() => this.askBanMember()} />
             </View>
         );
