3c0c8146e5d63b25932370081c0d0f29a1b24646
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2019-01-06 21:37:53 +0100

    Made a component for banned members to use in the banned members list of a group

---
+++ src/components/GroupMemberBannedComponent.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Text, View, Button, Alert } from 'react-native';
+import { postUnbanUser } from '../network/group';
+import Snackbar from 'react-native-snackbar';
+
+export default class GroupMemberBannedComponent extends React.Component {
+    askUnbanMember() {
+        Alert.alert(
+            'Unban member',
+            'Are you sure you want to unban ' + this.props.member.username + '?',
+            [
+                { text: 'Unban', onPress: () => this.banMember() },
+                { text: 'Cancel', style: 'cancel' }
+            ],
+            { cancelable: false }
+        );
+    }
+
+    async unbanMember() {
+        let response = await postUnbanUser(this.props.groupId, this.props.member.username, true);
+
+        if (response !== false) {
+            Snackbar.show({
+                title: this.props.member.username + ' was unbanned',
+                duration: Snackbar.LENGTH_LONG,
+                action: {
+                    title: 'CLOSE',
+                    color: 'green',
+                    onPress: () => Snackbar.dismiss(),
+                }
+            });
+            // TODO refresh banned members of group
+        }
+    }
+
+    render() {
+        return (
+            <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 5 }}>
+                <Text>{this.props.member.firstname} {this.props.member.lastname}</Text>
+                <Text style={{ flex: 1, marginLeft: 10 }}>({this.props.member.username})</Text>
+                <Button title="Unan" onPress={() => this.askUnbanMember()} />
+            </View>
+        );
+    }
+}
